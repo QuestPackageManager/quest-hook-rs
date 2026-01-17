58e3f50d589434b675218a864e8c69ff328a5d11
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-01-16 21:20:38 -0400

    Implement Type, Argument, Parameter, Returned, and Return traits for ValueTypePadding

--- libil2cpp/src/valuetype.rs
+++ libil2cpp/src/valuetype.rs
@@ -44,3 +44,73 @@ impl<const N: usize> Default for ValueTypePadding<N> {
         Self([0; N])
     }
 }
+
+// unsafe_impl_value_type!(in crate for ValueTypePadding<N> =>
+// System.ValueType);
+
+unsafe impl<const N: usize> crate::Type for ValueTypePadding<N> {
+    type Held<'a> = Self;
+    type HeldRaw = Self;
+    const NAMESPACE: &'static str = "System";
+    const CLASS_NAME: &'static str = "ValueType";
+    fn matches_value_argument(ty: &crate::Il2CppType) -> bool {
+        !ty.is_ref()
+            && ty
+                .class()
+                .is_assignable_from(<Self as crate::Type>::class())
+    }
+    fn matches_reference_argument(ty: &crate::Il2CppType) -> bool {
+        ty.is_ref()
+            && ty
+                .class()
+                .is_assignable_from(<Self as crate::Type>::class())
+    }
+    fn matches_value_parameter(ty: &crate::Il2CppType) -> bool {
+        !ty.is_ref() && <Self as crate::Type>::class().is_assignable_from(ty.class())
+    }
+    fn matches_reference_parameter(ty: &crate::Il2CppType) -> bool {
+        ty.is_ref() && <Self as crate::Type>::class().is_assignable_from(ty.class())
+    }
+}
+unsafe impl<const N: usize> crate::Argument for ValueTypePadding<N> {
+    type Type = Self;
+    fn matches(ty: &crate::Il2CppType) -> bool {
+        <Self as crate::Type>::matches_value_argument(ty)
+    }
+    fn invokable(&mut self) -> *mut ::std::ffi::c_void {
+        (self as *mut Self).cast::<::std::ffi::c_void>()
+    }
+}
+unsafe impl<const N: usize> crate::Parameter for ValueTypePadding<N> {
+    type Actual = Self;
+    fn matches(ty: &crate::Il2CppType) -> bool {
+        <Self as crate::Type>::matches_value_parameter(ty)
+    }
+    fn from_actual(actual: Self::Actual) -> Self {
+        actual
+    }
+    fn into_actual(self) -> Self::Actual {
+        self
+    }
+}
+unsafe impl<const N: usize> crate::Returned for ValueTypePadding<N> {
+    type Type = Self;
+    fn matches(ty: &crate::Il2CppType) -> bool {
+        <Self as crate::Type>::matches_returned(ty)
+    }
+    fn from_object(object: Option<&mut crate::Il2CppObject>) -> Self {
+        unsafe { crate::raw::unbox(crate::WrapRaw::raw(object.unwrap())) }
+    }
+}
+unsafe impl<const N: usize> crate::Return for ValueTypePadding<N> {
+    type Actual = Self;
+    fn matches(ty: &crate::Il2CppType) -> bool {
+        <Self as crate::Type>::matches_return(ty)
+    }
+    fn into_actual(self) -> Self::Actual {
+        self
+    }
+    fn from_actual(actual: Self::Actual) -> Self {
+        actual
+    }
+}
