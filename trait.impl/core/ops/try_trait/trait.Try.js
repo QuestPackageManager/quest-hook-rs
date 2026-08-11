e7a21d9decfd71d1e8f2b0cb24064c4986204c4f
Fernthedev <Fernthedev@users.noreply.github.com>
2026-08-11 03:51:10 +0000

    deploy: b7bb153590ef32870dd71bfc4fe2e294c3b49117

---
+++ trait.impl/core/ops/try_trait/trait.Try.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["libil2cpp",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/try_trait/trait.Try.html\" title=\"trait core::ops::try_trait::Try\">Try</a> for <a class=\"struct\" href=\"libil2cpp/struct.Gc.html\" title=\"struct libil2cpp::Gc\">Gc</a>&lt;T&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[286]}
